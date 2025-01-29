de963a0497f0d362040869bc49517eb658abc1d3
Bart Nagel <bart@tremby.net>
2025-01-29 10:52:55 -0800

    mutt: Add note

--- mail/.mutt/make-alternative.sh
+++ mail/.mutt/make-alternative.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Juice is https://www.npmjs.com/package/juice
+# So expects to be installed via `sudo npm i -g juice`
+
 juice <(pandoc --standalone --template=$HOME/.mutt/template.html --from=commonmark --to=html) /tmp/neomutt-alternative.html
